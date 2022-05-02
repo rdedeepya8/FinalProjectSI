de7cf7ad330e551ef3a22f1c591440f7cbe2eb79
rdedeepya8 <dedeepya.r1@gmail.com>
2022-05-01 20:55:00 -0400

    commit

--- Index.js
+++ Index.js
@@ -18,7 +18,7 @@ const options = {
         email: "draminen@uncc.edu",
       },
     },
-    host: "localhost:3000",
+    host: "http://137.184.115.225:3000",
     basePath: "/",
   },
   apis: ["./index.js"],
@@ -102,7 +102,7 @@ app.post("/Key_phrase", async (req, res) => {
     res.json(keyResults);
 
 })
-
+ 
 /**
  * @swagger
  * /PII:
